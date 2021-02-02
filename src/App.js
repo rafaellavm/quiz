0ce9613de1b4e037c3336004008a85ac9fbc66d2
Rafaela Marraschi <rafaela.vm@gmail.com>
2021-02-02 14:08:35 -0300

    13. Criando a lista de categorias

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Inicio from './Home/Inicio'
+import Categorias from './Jogo/Categoria'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <h1>Jogo de Perguntas e Respostas</h1>
       </header>
       <h1 className="App-intro">Bem-vindo</h1>
-      <Inicio />
+      <Categorias />
 
     </div>
 
